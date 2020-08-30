c4ec9f2c5e9d99c7ad258c942072ea3a6604a225
shaffannaeem123 <69971969+shaffannaeem123@users.noreply.github.com>
2020-08-30 15:35:50 +0500

    Update Car Accident Severity Project (IBM Capstone).py

--- Car Accident Severity Project (IBM Capstone).py
+++ Car Accident Severity Project (IBM Capstone).py
@@ -6,6 +6,21 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pip
 import scipy
+from sklearn import preprocessing
+import sm as sm
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier as KNC
+from sklearn import svm
+from sklearn.metrics import f1_score
+from sklearn.metrics import jaccard_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import log_loss
+import folium
+import webbrowser
+from folium import plugins
 
 #Reading the Data:
 import sm as sm
